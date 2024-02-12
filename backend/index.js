fca8efd41924df79aa9ce9398fd64a0925f8b097
Alexgg146 <sasha.kirov2015@yandex.ru>
2024-02-12 23:48:06 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,14 @@
-const nodemailer = require("nodemailer");
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
-const { errors } = require("celebrate");
-const userRouter = require("./router/user.router.js");
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(helmet());
-app.use(bodyParser.json());
-const port = 5000;
-app.use(
-  cors([
-    { origin: "http://localhost:3000", credentials: true },
-    { origin: "http://контрактпогран.рф", credentials: true },
-  ])
-);
-app.use(express.json());
-mongoose.connect("mongodb://127.0.0.1:27017/kontrakt");
-app.use("/users", userRouter);
+const nodemailer =require('nodemailer')
+const mongoose = require('mongoose');
+const userRouter = require('./router/user.router.js')
+const express = require('express')
+const cors=require('cors');
+const app = express()
+const port = process.env.port || 3001
+app.use(cors({origin:'https://контрактпогран.рф',credentials:true}));
+app.use(express.json())
+mongoose.connect('mongodb://127.0.0.1:27017/kontrakt', { useNewUrlParser: true, useUnifiedTopology: false });
+app.use('/',userRouter)
 app.listen(port, () => {
-  console.log(`Server good! http://localhost:${port}`);
-});
+    console.log(`Server good! http://localhost:${port}`)
+})
