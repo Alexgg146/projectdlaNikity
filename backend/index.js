55caa00ef65975650bf0dd66ca627bbd32105b1f
Alexgg146 <sasha.kirov2015@yandex.ru>
2024-02-12 22:42:06 +0300

    ljbihvkuo

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,27 @@
-const nodemailer =require('nodemailer')
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-const userRouter = require('./router/user.router.js')
-const express = require('express')
-const cors=require('cors');
-const app = express()
+const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const { errors } = require("celebrate");
+const userRouter = require("./router/user.router.js");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
-const port = 3001
-app.use(cors([{origin:'http://localhost:3000',credentials:true},{origin:'http://контрактпогран.рф',credentials:true}]));
-app.use(express.json())
-mongoose.connect('mongodb://127.0.0.1:27017/kontrakt', {  useNewUrlParser: true,
-useUnifiedTopology: true });
-app.use('/users',userRouter)
+const port = 3001;
+app.use(
+  cors([
+    { origin: "http://localhost:3000", credentials: true },
+    { origin: "http://контрактпогран.рф", credentials: true },
+  ])
+);
+app.use(express.json());
+mongoose.connect("mongodb://127.0.0.1:27017/kontrakt", {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+app.use("/users", userRouter);
 app.listen(port, () => {
-    console.log(`Server good! http://localhost:${port}`)
-})
+  console.log(`Server good! http://localhost:${port}`);
+});
